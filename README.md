9014657bb5fa646cf4ae0ef4458ee18141ebb49b
LindoK <khavin.lindo@gmail.com>
2019-06-21 15:03:21 -0400

    Completed game with notes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+
+A game where the app randomly picks a letter, and the user has to guess which letter the app chose. The user has 9 guesses. After 9 wrong guesses the game starts over.
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+
+
+var log = console.log;
+var letters = "abcdefghijklmnopqrstuvwxyz"
+
+var wins = 0;
+var losses = 0;
+
+var guessesLeft = 9;
+
+var Guesses = [];
+log(Guesses);
+
+function numberGenerator() {
+      var randomNumber = Math.floor(Math.random() * letters.length);
+      return randomNumber;
+}
+
+function letterGenerator() {
+    var number = numberGenerator();
+    var letter = letters[number];
+    return letter;
+}
+
+
+function psychicGame () {
+
+        document.onkeyup = function (event) 
+        {   
+            var randomLetter = letterGenerator();
+            var input = event.key;
+            
+            log(input);
+            if (input === randomLetter)
+            {
+                wins++;
+                Guesses.push(input)
+                document.getElementById("wins").innerHTML = "Wins: "+wins; 
+                document.getElementById("losses").innerHTML = "Losses: "+losses;  
+                document.getElementById("guesses-left").innerHTML = "Guesses Left: "+guessesLeft;
+                document.getElementById("guesses-so-far").innerHTML = "Your Guesses so far: "+Guesses;
+            }
+
+            if (input !== randomLetter){
+
+                losses++;
+                guessesLeft--;
+              
+                if (losses == 9 && guessesLeft == 0) 
+                {
+                    wins=0;
+                    losses=0;
+                    guessesLeft = 9;
+                    Guesses = [];
+
+                    document.getElementById("wins").innerHTML = "Wins: "+wins; 
+                    document.getElementById("losses").innerHTML = "Losses: "+losses;  
+                    document.getElementById("guesses-left").innerHTML = "Guesses Left: "+guessesLeft;
+                    document.getElementById("guesses-so-far").innerHTML = "Your Guesses so far: "+Guesses;
+                }
+               else {
+                    document.getElementById("wins").innerHTML = "Wins: "+wins; 
+                    document.getElementById("losses").innerHTML = "Losses: "+losses;  
+                    document.getElementById("guesses-left").innerHTML = "Guesses Left: "+guessesLeft;
+                    document.getElementById("guesses-so-far").innerHTML = "Your Guesses so far: "+Guesses;
+               }
+            }    
+        }
+    }
+
+
+psychicGame();
+
+
+
+
+
+   
+
